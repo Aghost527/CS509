8486b31e8d5e2474941c029c74c483e1a5bcf9ee
Aghost527 <240279329@qq.com>
2017-04-23 15:58:40 -0400

    2.7

--- src/ticket/TicketController.java
+++ src/ticket/TicketController.java
@@ -1,5 +1,8 @@
 package ticket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import flight.*;
 
 public class TicketController {
@@ -30,6 +33,45 @@ public class TicketController {
 		return true;
 
 	}
+	
+	public List<Tickets> noTicket(Flights flist, String seatType) {
+		Double price=0.0;
+		List<List<Ticket>> t=new ArrayList<List<Ticket>>();
+        List<Ticket> tt = new ArrayList<Ticket>(); 
+        t.add(new ArrayList<Ticket>(tt));
+		
+		for(int i=0;i<Math.pow(2, flist.size());i++){
+			int types=i;
+			boolean valid=true;
+//			Tickets ts=new Tickets();
+			for(Flight f:flist){
+				if((types&1)==1){
+					seatType = "First-Class";
+					String tprice=f.getSeating().getFirstClassPrice().replace(",", "");
+					price+=Double.valueOf(tprice.substring(1, tprice.length()));
+			        tt.add(new Ticket(f.getAirplane(), f.getNumber(), f.getArrival(), f.getDeparture(), seatType, tprice, f.getFlightTime(), f.getDeTimeString(), f.getArTimeString()));
+//			        t.add(new ArrayList<Ticket>(tt));	
+				}
+				else{
+					seatType = "First-Class";
+					String tprice=f.getSeating().getFirstClassPrice().replace(",", "");
+					price+=Double.valueOf(tprice.substring(1, tprice.length()));
+			        tt.add(new Ticket(f.getAirplane(), f.getNumber(), f.getArrival(), f.getDeparture(), seatType, tprice, f.getFlightTime(), f.getDeTimeString(), f.getArTimeString()));
+//			        t.add(new ArrayList<Ticket>(tt));
+				}
+				types=types>>1;
+		        t.add(new ArrayList<Ticket>(tt));
+
+			}
+			
+		}
+		
+		long diff = (flist.get(flist.size()-1).getArrivalTime().getTime() - flist.get(0).getDepartureTime().getTime()) / 60000;
+		return null;
+		
+		
+	
+}
 	
 
 }
